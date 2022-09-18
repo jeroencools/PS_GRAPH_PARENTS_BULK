4994f1599e2c0367ffe0ef092fc8b521f21a7fe9
jerrecools-vibo <113233490+jerrecools-vibo@users.noreply.github.com>
2022-09-18 19:39:55 +0200

    Add files via upload

---
+++ get_id.ps1
@@ -0,0 +1,23 @@
+﻿<# 
+Werkende met:
+1.11.1               Microsoft.Graph.Education  
+
+Voor connectie met MgGraph: https://learn.microsoft.com/en-us/powershell/microsoftgraph/get-started?view=graph-powershell-1.0 
+#>
+
+Import-Module Microsoft.Graph.Education
+
+Connect-MgGraph `
+    -ClientId "ZELF INVULLEN"
+    -TenantId "ZELF INVULLEN"
+    -CertificateThumbprint "ZELF INVULLEN"
+
+
+$csv = Import-Csv "ZELF INVULLEN"
+
+foreach($i in $csv)
+        {
+        $upn = $i.upn
+        Get-MgEducationUser -Filter "userPrincipalName eq '$upn'" | Export-Csv -Path "ZELF INVULLEN" -Append
+        } 
+
