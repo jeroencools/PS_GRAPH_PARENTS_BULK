896ba8ff83b29db61b872e98f7ded7a865e06333
Jeroen Cools <113233490+jeroencools@users.noreply.github.com>
2022-12-28 16:54:01 +0100

    Add files via upload

---
+++ edu_parents.ps1
@@ -0,0 +1,50 @@
+﻿Connect-MgGraph `
+    -ClientId "REPLACE WITH ID" `
+    -TenantId "REPLACE WITH ID" `
+    -CertificateThumbprint "REPLACE WITH THUMPRINT"
+
+
+
+Import-Module Microsoft.Graph.Education
+$csv = Import-Csv "$PSScriptRoot\data.csv"
+
+
+foreach($i in $csv)
+{
+      
+
+        $upncsv = $i.UPN
+        $user = Get-MgEducationUser -Filter "userPrincipalName eq '$upncsv'" 
+        $idcsv = $user.Id
+
+        $rc1namecsv = $i.rc1name
+        $rc1phonecsv = $i.rc1phone
+        $rc1mailcsv = $i.rc1mail
+        $rc2namecsv = $i.rc2name
+        $rc2phonecsv = $i.rc2phone
+        $rc2mailcsv = $i.rc2mail
+
+        $relatedcontacts = @{
+	        RelatedContacts = @(
+		        @{
+			    DisplayName = $rc1namecsv
+			    EmailAddress = $rc1mailcsv
+			    MobilePhone = $rc1phonecsv
+			    Relationship = "parent"
+			    AccessConsent = $true
+		        }
+	            @{
+			    DisplayName = $rc2namecsv
+			    EmailAddress = $rc2mailcsv
+			    MobilePhone = $rc2phonecsv
+			    Relationship = "parent"
+			    AccessConsent = $true
+		        }
+	            )
+}
+
+Update-MgEducationUser -EducationUserId $idcsv -BodyParameter $relatedcontacts
+
+}
+
+Write-Output "done!"
